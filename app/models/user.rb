052e1aca654297c14887c1425c43b86984293540
eagle1111 <wang499817054@gmail.com>
2017-07-28 15:21:56 +0800

    implement user can login on api

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
+  end
 end
